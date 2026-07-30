0771c1a22c1e6bc76edc0c250967fc6a6f572b57
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-07-30 15:03:50 +0530

    Fixed issues

--- Hotel.py
+++ Hotel.py
@@ -1,27 +1,39 @@
-Bookings=[["Aman","Standard",2500,5,0],["Vivek","Standard",2500,7,0],["Lalit","Luxury",6500,4,0]]
-for _ in Bookings[_]:
-  Bookings[_][-1]+=Bookings[_][-2]*Bookings[_][-3]
+Bookings = [
+    ["Aman", "Standard", 2500, 5, 0],
+    ["Vivek", "Standard", 2500, 7, 0],
+    ["Lalit", "Luxury", 6500, 4, 0]
+]
 
-totalrev=0
-for _ in Bookings[_]:
-  totalrev+=Bookings[_][-1]
-Standard=0
-Luxury=0
-for _ in Bookings[_]:
-  if Bookings[_][1]=="Standard":
-    Standard+=1
-  else :
-    Luxury +=1
-highest_Bill=0
-for _ in Bookings[_]:
-  if highest_Bill < Bookings[_][-1]:
-    highest_Bill=Bookings[_][-1]
-    highest=Bookings[_][0]
-for _ in Bookings[_]:
-  print(Bookings[_][0],":",Bookings[_][-1])
+# Calculate individual bills (Price * Days)
+for room in Bookings:
+    room[-1] += room[-2] * room[-3]
 
-print("Guest with Highest Bill : ",highest)
-print("No of Standard Rooms : ",Standard)
-print("No of Luxury Rooms : ",Luxury)
+# Calculate total revenue
+totalrev = 0
+for room in Bookings:
+    totalrev += room[-1]
 
-print("Total Revenue of Hotel : ",totalrev)
+# Count room types
+Standard = 0
+Luxury = 0
+for room in Bookings:
+    if room[1] == "Standard":
+        Standard += 1
+    else:
+        Luxury += 1
+
+
+highest_Bill = 0
+highest = ""
+for room in Bookings:
+    if highest_Bill < room[-1]:
+        highest_Bill = room[-1]
+        highest = room[0]
+
+for room in Bookings:
+    print(room[0], ":", room[-1])
+
+print("Guest with Highest Bill : ", highest)
+print("No of Standard Rooms : ", Standard)
+print("No of Luxury Rooms : ", Luxury)
+print("Total Revenue of Hotel : ", totalrev)
